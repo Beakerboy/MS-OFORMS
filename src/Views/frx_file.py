9ee235d6456b1033c606b69c0ee8483cdf786e07
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-08 15:28:31 -0400

    Create frx_file.py

---
+++ src/Views/frx_file.py
@@ -0,0 +1,12 @@
+class FRX:
+
+    @staticmethod
+    def from_file(path:str) -> 'FRX':
+        # open file
+        # read 24 byte header
+        # validade magic bytes
+        # validate size
+        # extract f-stream
+        # extract o-stream
+        # extract compobj stream
+        
