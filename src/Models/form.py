a7493e86506e553b4da4126992b7b89aa4a99754
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-08 12:52:59 -0400

    Create form.py

---
+++ src/Models/form.py
@@ -0,0 +1,19 @@
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='Form')
+
+
+class Form:
+
+    def __init__(self: T) -> None:
+        self._min_ver = 0
+        self._maj_ver = 4
+
+    def to_bytes(self: T) -> bytes:
+        output = struct.pack(
+            '<BB', self._min_ver, self._maj_ver
+        ) + self.data + self.extra +
+        self.stream_data + self.site_data +
+        self.design_ex_data
+        
