e191eb5879959b99e29f09abc8cfdfbd3555e500
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-09 10:49:07 -0400

    Update form_control.py

--- src/ms_oforms/Models/form_control.py
+++ src/ms_oforms/Models/form_control.py
@@ -117,7 +117,10 @@ class FormControl:
         output = struct.pack('<H', len(self.class_table))
         site_data = b''
         for site in self.sites:
-            site_data += b'\x00\x00SI'
+            site_data += (
+                struct.pack('<HH', 0, 4 + len(site[1]) + len(site[2])) +
+                site[0] + site[1] + site[2]
+            )
         depth = self.depth
         pad_size = min(4 - len(depth) % 4, 3)
         padded_depth = depth + b'\x00' * pad_size
