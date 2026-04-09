f85387d874738f5d1c268d6bf6a5480bb7b47ec1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-09 12:17:33 -0400

    Update test_form_control.py

--- tests/test_form_control.py
+++ tests/test_form_control.py
@@ -64,7 +64,7 @@ def test_to_bytes() -> None:
         "ClsidCacheIndex": b'\x00\x00',
         "GroupId": b'\x15\x00'
     }
-    site1_extra = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    site1_extra = b'Label1\x00\x00\x00\x00\x00\x00\x00\x00'
     form.sites = [
         (0x01f5, site1_data, site1_extra), (0, site1_data, b''), (0, site1_data, b''),
         (0, site1_data, b''), (0, site1_data, b'')
