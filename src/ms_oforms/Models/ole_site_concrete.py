ea2fd8f375586f3ad0c3d16d0ea5be031567aa3c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-09 19:08:19 -0400

    Create ole_site_concrete.py

---
+++ src/ms_oforms/Models/ole_site_concrete.py
@@ -0,0 +1,15 @@
+class OleSite:
+
+    SITE_PROP_MAP = {
+        0:  ("Name", "<I", DataLocation.BOTH),
+        1:  ("TagData", "<I", DataLocation.BOTH),
+        2:  ("ID", "<I", DataLocation.DATA_BLOCK),
+        3:  ("HelpContextId", "<I", DataLocation.DATA_BLOCK),
+        4:  ("BitFlags", "s", DataLocation.DATA_BLOCK),
+        5:  ("ObjectStreamSize", "<I", DataLocation.DATA_BLOCK),
+        6:  ("TabIndex", "<H", DataLocation.DATA_BLOCK),
+        7:  ("ClsidCacheIndex", "<H", DataLocation.DATA_BLOCK),
+        8:  ("Position", "<H", DataLocation.EXTRA_BLOCK),
+        9:  ("GroupId", "<H", DataLocation.DATA_BLOCK),
+        11: ("ControlTipText", "<I", DataLocation.BOTH)
+    }
