429127669bf1f2a3e927e0399c390f2d0ab171f6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-08 21:53:31 -0400

    Create test_form_control.py

---
+++ tests/test_form_control.py
@@ -0,0 +1,18 @@
+from ms_cfb.ole_file import OleFile
+from ms_oforms.Models.form_control import FormControl
+
+def test_to_bytes() -> None:
+    path1 = 'tests/files/Login.frx'
+    path2 = 'tests/files/Login.bin'
+    path3 = 'tests/files/f.bin'
+    with open(path1, 'rb') as file:
+        file.read(24)
+        ole_data = file.read()
+    with open(path2, 'wb') as file:
+        file.write(ole_data)
+    with open(path2, 'rb') as file:
+        olefile = OleFile.create_from_file(path2)
+        olefile.extract_stream('f', tests/files)
+    with open(path3, 'rb') as file:
+        expected = file.read
+        
