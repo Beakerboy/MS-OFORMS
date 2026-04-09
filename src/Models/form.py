52827d16291df62b425e24222abc0f70fb6bf0b3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-08 21:27:45 -0400

    Update form.py

--- src/Models/form.py
+++ src/Models/form.py
@@ -11,9 +11,12 @@ class Form:
         self._maj_ver = 4
 
     def to_bytes(self: T) -> bytes:
-        output = struct.pack(
-            '<BB', self._min_ver, self._maj_ver
-        ) + self.data + self.extra +
-        self.stream_data + self.site_data +
-        self.design_ex_data
+        output = (
+            struct.pack(
+                '<BB', self._min_ver, self._maj_ver
+            ) + self.data + self.extra +
+            self.stream_data + self.site_data +
+            self.design_ex_data
+        )
+        return output
         
