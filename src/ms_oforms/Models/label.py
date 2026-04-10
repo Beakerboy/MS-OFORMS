ab1029d5cc1016431a0dd50b41a332ca43f26d33
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-09 22:08:34 -0400

    Update label.py

--- src/ms_oforms/Models/label.py
+++ src/ms_oforms/Models/label.py
@@ -7,6 +7,19 @@ T = TypeVar('T', bound='Label')
 
 class Label:
 
+    LABEL_PROP_MAP = {
+        0:  ("ForeColor", "<I", DataLocation.DATA_BLOCK),
+        1:  ("BackColor", "<I", DataLocation.DATA_BLOCK),
+        2:  ("Various", "<I", DataLocation.DATA_BLOCK),
+        3:  ("Caption", "<I", DataLocation.BOTH),
+        4:  ("PicturePosition", "s", DataLocation.DATA_BLOCK),
+        5:  ("Size", "<I", DataLocation.EXTRA_BLOCK),
+        6:  ("MousePointer", "<H", DataLocation.DATA_BLOCK),
+        7:  ("BorderColor", "<H", DataLocation.DATA_BLOCK),
+        8:  ("BorderStyle", "<H", DataLocation.EXTRA_BLOCK),
+        9:  ("SpecialEffect", "<H", DataLocation.DATA_BLOCK),
+        11: ("Picture", "<I", DataLocation.BOTH)
+    } 
     def __init__(self: T) -> None:
         self.data = b''
         self.extended = b''
