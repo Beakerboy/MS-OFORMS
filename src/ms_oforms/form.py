66259178d3123df951ce21dcba11d9c5147ffd44
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-10 14:31:01 -0400

    Update form.py

--- src/ms_oforms/form.py
+++ src/ms_oforms/form.py
@@ -1,4 +1,5 @@
 from typing import TypeVar
+from ms_oforms.Models.control_base import ControlBase
 
 
 T = TypeVar('T', bound='Form')
@@ -7,7 +8,7 @@ T = TypeVar('T', bound='Form')
 class Form:
 
     def __init__(self: T) -> None:
-        self.objects = []
+        self.objects: list[ControlBase] = []
 
     def write_frx(self: T) -> None:
         # Create o-stream
