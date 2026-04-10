ac716df6ccee54536c8607116e4c3519e76b3960
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-10 08:48:08 -0400

    Create text_props.py

---
+++ src/ms_oforms/Models/text_props.py
@@ -0,0 +1,18 @@
+import struct
+from ms_oforms.Models.control_base import ControlBase
+from ms_oforms.Enums.data_location import DataLocation
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='TextProps')
+
+
+class TextProps(ControlBase):
+
+    PROP_MAP = {
+        0:  ("FontName", "<I", DataLocation.BOTH),
+        1:  ("FontEffects", "<I", DataLocation.DATA_BLOCK),
+        2:  ("FontHeight", "<I", DataLocation.DATA_BLOCK),
+        4:  ("FontCharset", "<I", DataLocation.DATA_BLOCK),
+        5:  ("FontPitchAndFamily", "s", DataLocation.DATA_BLOCK),
+    }
