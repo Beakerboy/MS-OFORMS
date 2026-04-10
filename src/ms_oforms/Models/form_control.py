cd76a1dae8497c584f0dc74c7b145d855b92a4ae
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-10 06:27:06 -0400

    Update form_control.py

--- src/ms_oforms/Models/form_control.py
+++ src/ms_oforms/Models/form_control.py
@@ -171,5 +171,10 @@ class FormControl:
             # If the property exists in the dict and is not None, set the bit
             if prop_name in self.properties and self.properties[prop_name] is not None:
                 mask |= (1 << bit)
-            
         return mask
+
+        @staticmethod
+        def compress_and_pad(value: bytes) -> bytes:
+            # if all the high bits are zero, remove them
+            # pad to 4 byte length
+            return value + b'\x00' * min(3, 4 - len(value) % 4)
