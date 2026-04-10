66cb8e27b2ca2ded6146648025e072f57afe234d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-10 13:59:03 -0400

    Update and rename tests/test_label.py to tests/Unit/test_label.py

--- tests/Unit/test_label.py
+++ tests/Unit/test_label.py
@@ -5,4 +5,4 @@ def test_compress() -> None:
     input = b'\xca\x05\x00\x00\xa7\x01\x00\x00'
     expected = input
     test_value = Label.compress_and_pad(input)
-    assert len(test_value) == 8
+    assert test_value == expected
