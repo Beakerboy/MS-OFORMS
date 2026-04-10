9034aeccc2e91959afeede18fa8429cefcdce7cd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-10 06:39:14 -0400

    Update test_label.py

--- tests/test_label.py
+++ tests/test_label.py
@@ -2,7 +2,6 @@ from ms_oforms.Models.label import Label
 
 
 def test_compress() -> None:
-    label = Label()
     input = b'Label1'
     expected = b'Label1\x00\x00'
     test_value = Label.compress_and_pad(input)
