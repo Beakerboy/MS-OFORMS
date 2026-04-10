51269ae084be3f278e25b01bd7841eb96e4828e2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-10 11:20:42 -0400

    Update test_form_embedded.py

--- tests/Functional/test_form_embedded.py
+++ tests/Functional/test_form_embedded.py
@@ -30,8 +30,8 @@ def test_to_bytes() -> None:
         "FontPitchAndFamily": 0x02
     }
     
-    textbox = MorphData()
-    textbox.properties = {
+    textbox1 = MorphData()
+    textbox2.properties = {
         "Various": b'\x1bH\x80,',
         "Size": b'\xb6\x0f\x00\x00\x7b\x02\x00\x00',
         "FontName": b'Tahoma',
@@ -50,8 +50,17 @@ def test_to_bytes() -> None:
         "FontPitchAndFamily": 0x02
     }
 
+    textbox2 = MorphData()
+    textbox2.properties = {
+        "Various": b'\x1bH\x80,',
+        "Size": b'\xb6\x0f\x00\x00\x7b\x02\x00\x00',
+        "FontName": b'Tahoma',
+        "FontHeight": 0xa5,
+        "FontCharset": 0x00,
+        "FontPitchAndFamily": 0x02
+    }
     command = CommandButton()
     
-    form.objects = [label1, textbox, label2, command]
+    form.objects = [label1, textbox1, label2, textbox2]
     
     assert form.to_bytes() == expected
