4b3a8e9751c874aa97bbb7887f55ea6bbd5a809b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-10 10:57:44 -0400

    Update text_props.py

--- src/ms_oforms/Models/text_props.py
+++ src/ms_oforms/Models/text_props.py
@@ -1,5 +1,5 @@
 import struct
-from ms_oforms.Models.control_base import ControlBase
+from ms_oforms.Models.structure_base import StructureBase
 from ms_oforms.Enums.data_location import DataLocation
 from typing import TypeVar
 
@@ -7,7 +7,7 @@ from typing import TypeVar
 T = TypeVar('T', bound='TextProps')
 
 
-class TextProps(ControlBase):
+class TextProps(StructureBase):
 
     PROP_MAP = {
         0:  ("FontName", "<I", DataLocation.BOTH),
