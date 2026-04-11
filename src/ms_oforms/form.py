82d60c5d28c16aa5c40068dce860c2eaa8bebe7e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-10 20:35:45 -0400

    Update form.py

--- src/ms_oforms/form.py
+++ src/ms_oforms/form.py
@@ -7,7 +7,12 @@ T = TypeVar('T', bound='Form')
 
 class Form:
 
+    default_props = {
+        "BooleanProperties": 4
+    }
+
     def __init__(self: T) -> None:
+        self._properties = self.default_props.copy()
         self._objects: list[ControlBase] = []
 
     def add_control(self: T, control: ControlBase) -> None:
@@ -16,6 +21,12 @@ class Form:
         control.properties["ObjectStreamSize"] = len(control.to_bytes())
         control.properties["ClsidCacheIndex"] = control.ClsidCacheIndex
         self._objects.append(control)
+
+    def enable_form(self: T) -> None:
+        self._properties["BooleanProperties"] |= 4
+
+    def disable_form(self: T) -> None:
+        self._properties["BooleanProperties"] ^= 4
 
     def write_frx(self: T) -> None:
         # Create o-stream
